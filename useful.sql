da8e66a41b29f9421d1de86d435abd9289e07778
Ali İhsan Karabal <aliihsankarabal@eskisehir.edu.tr>
2020-05-30 21:32:09 +0300

    Update useful.sql
    Added a new script

--- useful.sql
+++ useful.sql
@@ -1,3 +1,16 @@
+/* Son 24 saat içinde çalıştırılmış scriptleri listele */
+
+SELECT execquery.last_execution_time AS [Date Time]
+	,execsql.TEXT AS [Script]
+FROM sys.dm_exec_query_stats AS execquery
+CROSS APPLY 
+
+sys.dm_exec_sql_text(execquery.sql_handle) AS execsql
+ORDER BY execquery.last_execution_time DESC
+
+
+
+
 /* Deletes all user-defined stored procedures */
 
 Create proc delProcedures
